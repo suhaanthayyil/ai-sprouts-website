fa32a0aaa65aeb7819ac6004441974ecb2fea9e6
Kai <jupiterapps23@protonmail.com>
2026-08-05 09:57:26 -0400

    Speed up workshop photo carousel

--- components/home-carousel.tsx
+++ components/home-carousel.tsx
@@ -18,7 +18,7 @@ export function HomeCarousel() {
 
   useEffect(() => {
     if (window.matchMedia("(prefers-reduced-motion: reduce)").matches) return;
-    const timer = window.setInterval(() => setPosition((current) => current + 1), 5000);
+    const timer = window.setInterval(() => setPosition((current) => current + 1), 2000);
     return () => window.clearInterval(timer);
   }, []);
 
